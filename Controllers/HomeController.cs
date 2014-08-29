5fae73fc771eab15fdfa0c409997af31aaa2efde
1234901 <nic_pandolfo@hotmail.com>
2014-08-29 13:51:53 -0400

    Achat avec bouton non fonctionnel

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Il vous est possible de faire des frites";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Pourquoi pas des patates pilées?";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Bah! patate au four c'est bien correct";
+
+            return View();
+        }
+        public ActionResult Achat()
+        {
+            ViewBag.Message = "T'es un fou!";
+            return View();
+        }
+
+    }
+}
